392648ca632cd24010dee86fdb35ee2cc720fc14
gkatsaros16 <gkatsaros16@gmail.com>
2021-05-25 11:53:16 -0400

    removed details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
