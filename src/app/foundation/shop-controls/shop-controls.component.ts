759bb5c66d594a539248e5aa99eec6d3d5d32cd6
gkatsaros16 <gkatsaros16@gmail.com>
2021-05-25 13:04:20 -0400

    working on product detail/shop controls

---
+++ src/app/foundation/shop-controls/shop-controls.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-controls',
+  templateUrl: './shop-controls.component.html',
+  styleUrls: ['./shop-controls.component.scss']
+})
+export class ShopControlsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
